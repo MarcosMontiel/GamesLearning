e3b62191fcb3bd0e1f46c3135011ee76632cfafe
Marcos Montiel <marcos-gab14@hotmail.com>
2023-05-25 15:42:04 -0600

    Floating action button file was modified

--- app/src/main/java/com/marcosmontiel/gameslearning/presentation/components/DefaultFloatingActionButton.kt
+++ app/src/main/java/com/marcosmontiel/gameslearning/presentation/components/DefaultFloatingActionButton.kt
@@ -5,12 +5,15 @@ import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
+import com.marcosmontiel.gameslearning.presentation.ui.theme.Blue500
 
 @Composable
 fun DefaultFloatingActionButton(
     modifier: Modifier = Modifier,
+    background: Color = Blue500,
     icon: ImageVector,
     onClickAction: () -> Unit
 ) {
@@ -19,6 +22,7 @@ fun DefaultFloatingActionButton(
             onClickAction()
         },
         modifier = modifier.padding(bottom = 56.dp),
+        backgroundColor = background,
         content = {
 
             Icon(
