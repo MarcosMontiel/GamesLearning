2d3b9ec9070714a8fb9311286e691bb7ed228cb2
Marcos Montiel <marcos-gab14@hotmail.com>
2023-06-01 13:32:34 -0600

    Posts content was modified

--- app/src/main/java/com/marcosmontiel/gameslearning/presentation/screens/posts/components/PostsContent.kt
+++ app/src/main/java/com/marcosmontiel/gameslearning/presentation/screens/posts/components/PostsContent.kt
@@ -1,18 +1,20 @@
 package com.marcosmontiel.gameslearning.presentation.screens.posts.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.marcosmontiel.gameslearning.domain.model.Post
+import com.marcosmontiel.gameslearning.presentation.components.DefaultAsyncImage
 import com.marcosmontiel.gameslearning.presentation.screens.posts.PostsViewModel
+import com.marcosmontiel.gameslearning.presentation.ui.theme.Gray800
 
 @Composable
 fun PostsContent(
@@ -33,7 +35,8 @@ fun PostsContent(
                     items(items = posts) { post ->
 
                         PostCard(
-                            modifier = Modifier,
+                            modifier = Modifier.fillMaxWidth(),
+                            background = Gray800,
                             post = post
                         )
 
@@ -47,6 +50,24 @@ fun PostsContent(
 }
 
 @Composable
-fun PostCard(modifier: Modifier, post: Post) {
-    Text(text = post.name)
+fun PostCard(modifier: Modifier, background: Color, post: Post) {
+    Card(
+        modifier = modifier.padding(20.dp),
+        shape = RoundedCornerShape(16.dp),
+        backgroundColor = background,
+        elevation = 4.dp
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
+
+            DefaultAsyncImage(
+                modifier = Modifier.size(80.dp),
+                image = post.image
+            )
+
+        }
+    }
 }
