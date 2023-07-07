d67922ba81faef208104718745ad2a13fa5d871f
Marcos Montiel <marcos-gab14@hotmail.com>
2023-07-07 17:10:12 -0600

    Post detail files were modified

--- app/src/main/java/com/marcosmontiel/gameslearning/presentation/screens/posts/PostsViewModel.kt
+++ app/src/main/java/com/marcosmontiel/gameslearning/presentation/screens/posts/PostsViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.marcosmontiel.gameslearning.domain.model.Post
 import com.marcosmontiel.gameslearning.domain.model.Response
+import com.marcosmontiel.gameslearning.domain.model.User
 import com.marcosmontiel.gameslearning.domain.usecase.post.PostUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -31,27 +32,24 @@ class PostsViewModel @Inject constructor(
 
     fun convertPostToJson(post: Post): String {
 
-        val userArgument = post.user?.let { data ->
-            if (data.avatar.isNotEmpty()) {
-                val encoded = URLEncoder.encode(data.avatar, StandardCharsets.UTF_8.toString())
-                data.copy(avatar = encoded)
-            } else {
-                data
-            }
+        val encodedUser: User = post.user?.let { data ->
+            data.copy(
+                avatar = imageEncoder(data.avatar),
+                email = stringEncoder(data.email),
+                username = stringEncoder(data.username),
+            )
+        } ?: User()
+
+        val encodedPost: Post = post.let { data ->
+            data.copy(
+                description = stringEncoder(data.description),
+                image = imageEncoder(data.image),
+                name = stringEncoder(data.name),
+                user = encodedUser,
+            )
         }
 
-        val postArgument = post.let { data ->
-
-            if (data.image.isNotEmpty()) {
-                val encoded = URLEncoder.encode(data.image, StandardCharsets.UTF_8.toString())
-                data.copy(image = encoded, user = userArgument)
-            } else {
-                data.copy(user = userArgument)
-            }
-
-        }
-
-        return postArgument.toJson()
+        return encodedPost.toJson()
 
     }
 
@@ -61,6 +59,29 @@ class PostsViewModel @Inject constructor(
         postsResponse = Response.Loading
         postUseCases.getPosts().collect { response ->
             postsResponse = response
+        }
+    }
+
+    private fun imageEncoder(data: String): String =
+        if (data.isEmpty()) data else URLEncoder.encode(data, StandardCharsets.UTF_8.toString())
+
+    private fun stringEncoder(data: String): String {
+        val regex = buildString { append("[?=&#+%/.; ]") }
+
+        return data.replace(Regex(regex)) { matchResult ->
+            when (matchResult.value) {
+                "?" -> "%3F"
+                "=" -> "%3D"
+                "&" -> "%26"
+                "#" -> "%23"
+                "+" -> "%2B"
+                "%" -> "%25"
+                "/" -> "%2F"
+                "." -> "%2E"
+                ";" -> "%3B"
+                " " -> "%20"
+                else -> matchResult.value
+            }
         }
     }
 
